764ede1e5c7be5d7c36ee6dbddb24b39350da3f6
Debashish D. Dev <hello@debashish.dev>
2022-04-12 10:09:54 +0600

    first commit

---
+++ show-product-description.php
@@ -0,0 +1,10 @@
+<?php
+    /*
+     * Plugin Name: MKD Shorts
+     * Description: MKD Plugin 1
+     * Version: 1.0
+     */
+
+    function mkd_short_des_product() {
+        the_excerpt();
+   }
